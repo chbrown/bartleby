d4bcad5fc78a747630f02c3489bb8723bf96e443
Christopher Brown <io@henrian.com>
2016-11-14 17:33:47 -0600

    Add lein-cloverage plugin, Travis-CI, and Coveralls integration

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
 (defproject bartleby "0.1.0-SNAPSHOT"
   :description "Faithful (Bib)TeX manipulation"
   :url "https://github.com/chbrown/bartleby"
-  :license {:name "MIT", :url "https://chbrown.github.io/licenses/MIT/#2016"}
+  :license {:name "MIT"
+            :url "https://chbrown.github.io/licenses/MIT/#2016"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.293"]
                  [org.clojure/data.json "0.2.6"]
                  [the/parsatron "0.0.7"]]
-  :plugins [[lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]]
+  :plugins [[lein-cloverage "1.0.9"]
+            [lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]]
   :profiles {:dev {:source-paths ["dev" "test"]
                    :resource-paths ["test/resources"]
                    :dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]
