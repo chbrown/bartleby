58fcee75beb3eee297702876931a7906cf69bdef
Christopher Brown <io@henrian.com>
2016-12-17 17:23:26 -0600

    Re-include lein-cloverage plugin to fix Travis CI break

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [the/parsatron "0.0.7"]]
+  :plugins [; have to include lein-cloverage here for Travis CI
+            [lein-cloverage "1.0.9"]]
   :main bartleby.cli
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["dev" "test"]
