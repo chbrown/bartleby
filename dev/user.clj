2fc7d752543ae9e9107f6208d93201ba12e6bac2
Christopher Brown <io@henrian.com>
2017-01-04 10:23:00 -0600

    Prune dev/user.clj

--- dev/user.clj
+++ dev/user.clj
@@ -9,11 +9,8 @@
             [bartleby.core :as core]
             [bartleby.language.bibtex :as bibtex]
             [bartleby.language.tex :as tex]
-            ; [bartleby.language.common :as parsers]
-            [clojure.tools.namespace.repl :refer [refresh refresh-all]])
+            [bartleby.language.common :as common])
   (:gen-class))
-
-(def examplevalue "{Description (in SLF) de la dur{\\'e}e Fran{\\c{c}}aise {\\`a} partir d'une [in French]}")
 
 (defn -main
   []
