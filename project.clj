5d4f36d036cfb8fdbe3c124731c200dd3c87163a
Christopher Brown <io@henrian.com>
2017-01-19 13:45:16 -0600

    Bump version by patch

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bartleby "0.2.0-SNAPSHOT"
+(defproject bartleby "0.2.1-SNAPSHOT"
   :description "Faithful (Bib)TeX manipulation"
   :url "https://github.com/chbrown/bartleby"
   :license {:name "MIT"
