be8c04737136ec0464534188f0d20fbd95cc30a3
Christopher Brown <io@henrian.com>
2017-03-23 12:44:57 -0500

    Add codox plugin and config

--- project.clj
+++ project.clj
@@ -8,10 +8,13 @@
                  [org.clojure/tools.cli "0.3.5"]
                  [the/parsatron "0.0.7"]]
   :plugins [; have to include lein-cloverage here for Travis CI
-            [lein-cloverage "1.0.9"]]
+            [lein-cloverage "1.0.9"]
+            [lein-codox "0.10.3"]]
   :main bartleby.cli
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :sign-releases false}]]
+  :codox {:source-paths ["src"] ; exclude dev/ and test/
+          :source-uri "https://github.com/chbrown/bartleby/blob/v{version}/{filepath}#L{line}"}
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["dev" "test"]
                    :resource-paths ["test/resources"]
