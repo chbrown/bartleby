593eb31515053fcb73c7fe79d8f4f0b88fefef42
Christopher Brown <io@henrian.com>
2017-07-15 22:45:16 -0500

    Bump minor version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bartleby/bartleby "0.8.1"
+(defproject bartleby/bartleby "0.9.0"
   :description "Faithful (Bib)TeX manipulation"
   :url "https://github.com/chbrown/bartleby"
   :license {:name "MIT"
