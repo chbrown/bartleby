e516aaf9adc87bb39bc7e9284d44d3f1a1e04dea
Christopher Brown <io@henrian.com>
2017-12-10 13:57:23 -0600

    Move codox plugin and config into :dev profile

--- project.clj
+++ project.clj
@@ -9,15 +9,15 @@
                  [org.clojure/tools.cli "0.3.5"]
                  [chbrown/parsatron "0.0.8"]]
   :plugins [; have to include lein-cloverage here for Travis CI
-            [lein-cloverage "1.0.9"]
-            [lein-codox "0.10.3"]]
+            [lein-cloverage "1.0.9"]]
   :main bartleby.cli
   :deploy-repositories [["releases" :clojars]]
-  :codox {:source-paths ["src"] ; exclude dev/ and test/
-          :source-uri "https://github.com/chbrown/bartleby/blob/v{version}/{filepath}#L{line}"}
   :profiles {:uberjar {:aot :all}
-             :dev {:source-paths ["dev" "test"]
+             :dev {:plugins [[lein-codox "0.10.3"]]
+                   :source-paths ["dev" "test"]
                    :resource-paths ["test/resources"]
+                   :codox {:source-paths ["src"] ; exclude dev/ and test/
+                           :source-uri "https://github.com/chbrown/bartleby/blob/v{version}/{filepath}#L{line}"}
                    :dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]
                                   [org.clojure/tools.trace "0.7.9"]]
                    :repl-options {:init-ns user}}
