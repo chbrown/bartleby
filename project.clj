4777493528934ed9ee71da274a6b2732d61e3ee8
Christopher Brown <io@henrian.com>
2017-12-10 13:59:16 -0600

    Move cloverage plugin into :test profile

--- project.clj
+++ project.clj
@@ -8,8 +8,6 @@
                  [org.clojure/data.xml "0.2.0-alpha2"]
                  [org.clojure/tools.cli "0.3.5"]
                  [chbrown/parsatron "0.0.8"]]
-  :plugins [; have to include lein-cloverage here for Travis CI
-            [lein-cloverage "1.0.9"]]
   :main bartleby.cli
   :deploy-repositories [["releases" :clojars]]
   :profiles {:uberjar {:aot :all}
@@ -21,5 +19,6 @@
                    :dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]
                                   [org.clojure/tools.trace "0.7.9"]]
                    :repl-options {:init-ns user}}
-             :test {:source-paths ["test"]
+             :test {:plugins [[lein-cloverage "1.0.10"]]
+                    :source-paths ["test"]
                     :resource-paths ["test/resources"]}})
