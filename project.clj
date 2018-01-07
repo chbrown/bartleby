70c9879232bdf3e662e1de60b16706badcad2224
Christopher Brown <io@henrian.com>
2018-01-06 18:52:15 -0600

    Add cljs support and cljs testing via doo (not yet passing)

--- project.clj
+++ project.clj
@@ -3,22 +3,30 @@
   :url "https://github.com/chbrown/bartleby"
   :license {:name "MIT"
             :url "https://chbrown.github.io/licenses/MIT/#2016-2017"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.946"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.2.0-alpha2"]
                  [org.clojure/tools.cli "0.3.5"]
                  [chbrown/parsatron "0.0.8"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
   :main bartleby.cli
-  :deploy-repositories [["releases" :clojars]]
+  :cljsbuild {:builds [{:id "test"
+                        :source-paths ["src" "test"]
+                        :compiler {:output-dir "target/test"
+                                   :output-to "target/test/main.js"
+                                   :main bartleby.runner
+                                   :optimizations :whitespace}}]}
   :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[lein-codox "0.10.3"]]
-                   :source-paths ["dev" "test"]
+             :dev {:plugins [[lein-codox "0.10.3"]
+                             [lein-cloverage "1.0.10"]
+                             [lein-doo "0.1.8"]]
+                   :doo {:paths {:rhino "lein run -m org.mozilla.javascript.tools.shell.Main"}}
                    :resource-paths ["test/resources"]
                    :codox {:source-paths ["src"] ; exclude dev/ and test/
-                           :source-uri "https://github.com/chbrown/bartleby/blob/v{version}/{filepath}#L{line}"}
-                   :dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]
-                                  [org.clojure/tools.trace "0.7.9"]]
-                   :repl-options {:init-ns user}}
-             :test {:plugins [[lein-cloverage "1.0.10"]]
-                    :source-paths ["test"]
-                    :resource-paths ["test/resources"]}})
+                           :source-uri "https://github.com/chbrown/bartleby/blob/v{version}/{filepath}#L{line}"}}
+             :repl {:dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]
+                                   [org.clojure/tools.trace "0.7.9"]]
+                    :source-paths ["dev"]
+                    :repl-options {:init-ns user}}})
---
+++ test/bartleby/runner.cljs
@@ -0,0 +1,10 @@
+(ns bartleby.runner
+  (:require [doo.runner :refer-macros [doo-all-tests]]
+            [bartleby.test.bibliography]
+            [bartleby.test.bibtex]
+            [bartleby.test.core]
+            [bartleby.test.jats]
+            [bartleby.test.json]
+            [bartleby.test.tex]))
+
+(doo-all-tests)
